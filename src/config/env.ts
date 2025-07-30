25fd5683a7ee55df04a66348d1f9c93bae58d8a1
OleanjiKingCode <holamilean56@gmail.com>
2025-07-30 19:18:47 +0100

    Update rollbackManagerAddress in environment configuration

--- src/config/env.ts
+++ src/config/env.ts
@@ -2,7 +2,7 @@ export const config = {
   apiUrl:
     import.meta.env.VITE_API_URL ||
     "https://rollback-service.onrender.com/api/v1",
-  rollbackManagerAddress:"0x030d177478aE41676D715AB4466557F37E9ab19A",
+  rollbackManagerAddress: "0x7aFA0a3063CC49898da29C8CF5275DB13146aB6A",
   reownProjectId: import.meta.env.VITE_REOWN_PROJECT_ID || "",
 } as const;
 
